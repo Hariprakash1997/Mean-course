6da6527dd3aea4e921bdba90d4f74fde961a76f0
Hariprakash1997 <1997hariprakash@gmail.com>
2018-10-15 12:40:39 +0530

    deleted backend

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const debug = require('debug')('node-angular');
-const app = express();
-
-app.use((req, res, next) => {
-  console.log("First Middleware");
-});
-
-module.exports = app;
